cb64b90238f247b779abaa52896b44749fce4250
unknown <husseinfawzy77@outlook.com>
2023-03-03 16:49:48 +0200

    update file

--- solar.py
+++ solar.py
@@ -1,5 +1,6 @@
 def main():
     import pandas as pd
+    import matplotlib.pyplot as plt
     import pvlib
 
     from pvlib.modelchain import ModelChain
@@ -13,12 +14,23 @@ def main():
     cec_inverters = pvlib.pvsystem.retrieve_sam('CECInverter')
 
     module = sandia_modules['Canadian_Solar_CS5P_220M___2009_']
-    inverter = cec_inverters['ABB__MICRO_0_25_I_OUTD_US_208__208V_']
+    inverter = cec_inverters['iPower__SHO_5_2__240V_']
     temperature_parameters = TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_glass']
     system =PVSystem(surface_tilt=45, surface_azimuth=180, 
                      module_parameters=module, inverter_parameters=inverter, 
-                     temperature_model_parameters=temperature_parameters)
-    #print(system)
+                     temperature_model_parameters=temperature_parameters,
+                     modules_per_string=10, strings_per_inverter=3)
+    #print(module['Impo'] * module['Vmpo'] * 10)
+    #print(inverter)
     modelchain = ModelChain(system,location)
-    print(modelchain)
+    #print(modelchain)
+    times = pd.date_range(start='2021-07-01', end='2021-07-02', freq='30min', tz=location.tz)
+    clear_sky = location.get_clearsky(times)
+
+    clear_sky.plot(figsize=(9,4))
+    plt.show()
+
+    modelchain.run_model(clear_sky)
+    modelchain.results.dc.plot(figsize=(9,4))
+    plt.show()
 main()
